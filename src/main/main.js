7867b2a0d5d0e5444f51a527e35d7b86a3dd34bc
Jonathan Moo <250204001@escarcega.tecnm.mx>
2026-06-01 12:25:28 -0600

    Feat: Initialize TaskPet Desktop App

---
+++ src/main/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 300,
+        height: 300,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }    
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
