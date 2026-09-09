cc556ec8c455a6af9fce8367a13b47748ed1229f
Jonathan Moo <250204001@escarcega.tecnm.mx>
2026-09-08 19:10:02 -0600

    Feat: Ventana minima con scheluder que revisa recordatorios cada minuto y dispara notificaciones masivas

--- src/Test/testTask.js
+++ src/Test/testTask.js
@@ -1,19 +1,4 @@
-const {addTask, getTasks, completeTask, deleteTask, isTaskDue, markReminded} = require("../services/TaskManager.js");
+const { addTask } = require("../services/TaskManager.js");
 
-const nueva = addTask({text: "Probar TaskManager", reminderMinutes: 0});
-console.log("Tarea creada: ", nueva);
-
-console.log("Todas las tareas: ", getTasks());
-console.log("Tareas Aviso: ", isTaskDue(nueva))
-
-// const completada = completeTask();
-// console.log("Tareas Completadas: ", completada);
-
-// const borrar = deleteTask();
-// console.log("Tareas Eliminadas: ", borrar);
-
-const recordatorio = markReminded(nueva.id)
-console.log("Recordatorios", recordatorio)
-
-console.log("Tareas Aviso: ", isTaskDue(recordatorio))
-
+const nueva = addTask({ text: "Probar notificación real", reminderMinutes: 0 });
+console.log("Tarea creada:", nueva);
